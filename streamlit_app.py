aa76e061ad6d96992faa212656d9b9239f51f9bf
Akanksha Salunkhe <68583036+Akanksha-5-9@users.noreply.github.com>
2024-09-09 10:17:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,50 @@
-# Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import snowflake.connector
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
-st.write(
-    """Choose the fruits you want in your custom smoothie!
-    """)
-
-import streamlit as st
+st.write("Choose the fruits you want in your custom smoothie!")
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-cnx = st.connection("Snowflake")
-session = cnx.session()
+# Accessing Snowflake credentials from secrets
+snowflake_credentials = st.secrets["connections"]["snowflake"]
 
+# Establishing connection to Snowflake
+conn = snowflake.connector.connect(
+    user=snowflake_credentials["SALUNKHEAKANKSHA23"],
+    password=snowflake_credentials["HariOm@23"],
+    account=snowflake_credentials["BNYSQPU-ID92342"],
+    warehouse=snowflake_credentials["COMPUTE_WH"],
+    database=snowflake_credentials["SMOOTHIES"],
+    schema=snowflake_credentials["PUBLIC"],
+    role=snowflake_credentials["ACCOUNTADMIN"],
+    client_session_keep_alive=snowflake_credentials["client_session_keep_alive"]
+)
+
+session = conn.cursor()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe,
-    max_selections = 5
+    max_selections=5
 )
 
 if ingredients_list:
-    ingredients_string = ''
+    ingredients_string = ', '.join(ingredients_list)
 
-    for fruit_choosen in ingredients_list:
-        ingredients_string += fruit_choosen
+    my_insert_stmt = f"""
+    INSERT INTO smoothies.public.orders (ingredients, name_on_order)
+    VALUES ('{ingredients_string}', '{name_on_order}')
+    """
 
-    #st.write(ingredients_string)
-
-    
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
-
-    #st.write(my_insert_stmt)
-    #st.stop()
-    
     time_to_insert = st.button('Submit Order')
-    
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        
-        st.success(f'Your Smoothie is ordered, {name_on_order}!',icon="✅")
 
+    if time_to_insert:
+        session.execute(my_insert_stmt)
+        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
